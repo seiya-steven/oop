f521121c7f2a435f3a53f6976a7407f3e072cc5d
Hoang Thang <hoangthang12102006@gmail.com>
2025-09-21 13:52:08 +0700

    Up file introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,30 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println( "Hoang Van Thang\t" +
+                            "24020307\t"        +
+                            "IT1\t"             +
+                            "HoangThang"        +
+                            "hoangthang121006@gmail.com"
+                          );
+
+        System.out.println(
+                            "9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                            "\n" +
+                            "Take one down, pass it around, \n" +
+                            "\n" +
+                            "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                            "\n" +
+                            "Take one down, pass it around,\n" +
+                            "\n" +
+                            "...\n" +
+                            "\n" +
+                            "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                            "\n" +
+                            "Take one down, pass it around,\n" +
+                            "\n" +
+                            "No more bottles of beer on the wall."
+                        );
+    }
+
+}
